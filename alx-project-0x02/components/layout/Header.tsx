2224123e0dbea3340e6e9edb4ed2dc1de1950648
Benjamin Lamptey <kbenlamptey@gmail.com>
2025-11-22 13:33:00 +0000

    Basic routing

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import Link from "next/link";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="container bg-amber-400 ">
+      <nav className="navbar flex space-x-4 p-4">
+        <ul className="flex justify-between gap-6">
+          <li className="">
+            <Link href="/home">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
